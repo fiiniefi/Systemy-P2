bf74f283e934f4e5f931c6636c937f913808e0aa
fiiniefi <marcin.drewniak1@vp.pl>
2017-11-26 19:28:19 +0100

    Files added

---
+++ makefile
@@ -0,0 +1,12 @@
+CC=g++ -std=c++11
+CFLAGS=-Wall -Werror
+LIBS=-pthread -time
+
+settlers: settlers.o
+	$(CC) settlers.o -o settlers $(LIBS)
+
+main.o: settlers.cpp
+	$(CC) $(CFLAGS) settlers.cpp -c -o settlers.o
+
+run: settlers
+	./settlers 10 11 3 7
---
+++ settlers.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+//#include <thread>
+
+int hunters_numb, cooks_numb, meat_numb, meal_numb;
+void *hunter_routine(void *);
+void *cook_routine(void *);
+void consume(int &);
+
+
+void *hunter_routine(void *)
+{
+    srand(time(NULL));
+    if (std::rand() % 6 > std::rand() % 6) //first for hunter, second for prey
+        meat_numb++;
+    consume(hunters_numb);
+    pthread_exit(NULL);
+}
+
+
+void *cook_routine(void *)
+{
+    if (meat_numb >= 0)
+    {
+        meat_numb--;
+        meal_numb++;
+    }
+    consume(cooks_numb);
+    pthread_exit(NULL);
+}
+
+void consume(int &villagers_numb)
+{
+    if (meal_numb >= 0)
+        meal_numb--;
+    else if (villagers_numb >= 0)
+        villagers_numb--;
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 5)
+    {
+        std::cout << "Program " << argv[0] << " needs 4 arguments: hunters_numb, cooks_numb, meat_numb, meal_numb" << std::endl;
+        return 0;
+    }
+  
+    hunters_numb = atoi(argv[1]), cooks_numb = atoi(argv[2]);
+    meat_numb = atoi(argv[3]), meal_numb = atoi(argv[4]);
+    
+    pthread_t hunters[hunters_numb];
+    pthread_t cooks[cooks_numb];
+
+    for (int i = 1 ; i <= 100 ; i ++)
+    {
+        for (int j = 0 ; j < hunters_numb ; j++)
+        {
+            int flag = pthread_create(&hunters[j], NULL, hunter_routine, NULL);
+            if (flag)
+                std::cout << "Error: Hunter thread cannot be created" << std::endl;
+        }
+
+        for (int j = 0 ; j < cooks_numb ; j++)
+        {
+            int flag = pthread_create(&cooks[j], NULL, cook_routine, NULL);
+            if (flag)
+                std::cout<< "Error: Cook thread cannot be created" << std::endl;
+        }
+        //usleep(1000);
+        std::cout << "Number of hunters after " << i << " day(s): " << hunters_numb << std::endl;
+        std::cout << "Number of cooks after " << i << " day(s): " << cooks_numb << std::endl;
+        std::cout << "Number of meat after " << i << " day(s): " << meat_numb << std::endl;
+        std::cout << "Number of meal after " << i << " day(s): " << meal_numb << std::endl;
+    }
+}
